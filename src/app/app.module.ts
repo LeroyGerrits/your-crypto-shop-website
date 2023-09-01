656711167dcf1f079cc4b8e8311dfa774100f570
Leroy Gerrits <leroygerrits@gmail.com>
2023-09-02 01:35:49 +0200

    Financial statement upgrades

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -34,6 +34,7 @@ import { MatBadgeModule } from '@angular/material/badge';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatChipsModule } from '@angular/material/chips';
+import {MatDatepickerModule} from '@angular/material/datepicker';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatGridListModule } from '@angular/material/grid-list';
@@ -41,6 +42,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatNativeDateModule } from '@angular/material/core';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSelectModule } from '@angular/material/select';
@@ -99,15 +101,17 @@ import { ShopService } from './shared/services/Shop.service';
     HttpClientModule,
     MatBadgeModule,
     MatCardModule,
-    MatChipsModule,
+    MatChipsModule,    
+    MatDatepickerModule,
     MatDialogModule,
     MatButtonModule,
     MatFormFieldModule,
     MatGridListModule,
     MatIconModule,
     MatInputModule,
-    MatListModule,
+    MatListModule,    
     MatMenuModule,
+    MatNativeDateModule,
     MatPaginatorModule,
     MatProgressSpinnerModule,
     MatSelectModule,
--- src/app/public-website/financial-statement/financial-statement.component.ts
+++ src/app/public-website/financial-statement/financial-statement.component.ts
@@ -3,6 +3,8 @@ import { FormControl, FormGroup } from '@angular/forms';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort, Sort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
+import { FinancialStatementTransactionType } from 'src/app/shared/enums/FinancialStatementTransactionType.enum';
+import { Recurrance } from 'src/app/shared/enums/Recurrance.enum';
 import { Environment } from 'src/app/shared/environments/Environment';
 import { Currency } from 'src/app/shared/models/Currency.model';
 import { FinancialStatementTransaction } from 'src/app/shared/models/FinancialStatementTransaction.model';
@@ -11,20 +13,27 @@ import { FinancialStatementTransactionService } from 'src/app/shared/services/Fi
 
 @Component({
   selector: 'public-website-financial-statement',
-  templateUrl: './financial-statement.component.html'
+  templateUrl: './financial-statement.component.html',
+  styleUrls: ['./financial-statement.component.scss']
 })
 
 export class PublicWebsiteFinancialStatementComponent implements OnInit {
   @ViewChild(MatPaginator, { static: false }) paginator!: MatPaginator;
   @ViewChild(MatSort, { static: false }) sort!: MatSort;
+  financialStatementTransactionTypes = Object.keys(FinancialStatementTransactionType).filter(p => isNaN(p as any));
+  recurrances = Object.keys(Recurrance).filter(p => isNaN(p as any));
 
   environment = Environment;
   dataSource = new MatTableDataSource<FinancialStatementTransaction>;
-  displayedColumns: string[] = ['Date', 'Type', 'Amount', 'Currency', 'Description', 'Recurrance'];
+  displayedColumns: string[] = ['Date', 'Description', 'Recurrance', 'Type', 'Currency', 'Amount'];
   sortDirection: string | null = 'asc';
 
   public form!: FormGroup;
+  public controlFilterDateFrom = new FormControl('');
+  public controlFilterDateUntil = new FormControl('');
   public controlFilterDescription = new FormControl('');
+  public controlFilterRecurrance = new FormControl('');
+  public controlFilterType = new FormControl('');
   public controlFilterCurrency = new FormControl('');
   public currencies: Currency[] | undefined;
 
@@ -33,7 +42,11 @@ export class PublicWebsiteFinancialStatementComponent implements OnInit {
     private currencyService: CurrencyService
   ) {
     this.form = new FormGroup([
+      this.controlFilterDateFrom,
+      this.controlFilterDateUntil,
       this.controlFilterDescription,
+      this.controlFilterRecurrance,
+      this.controlFilterType,
       this.controlFilterCurrency
     ]);
   }
