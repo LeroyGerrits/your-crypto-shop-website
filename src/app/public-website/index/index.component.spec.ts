a209c5a8832b86161d3e659c365e8aac517691c0
Leroy Gerrits <leroygerrits@gmail.com>
2023-09-15 20:49:11 +0200

    Fix unit test

--- src/app/public-website/index/index.component.spec.ts
+++ src/app/public-website/index/index.component.spec.ts
@@ -1,14 +1,24 @@
+import { ActivatedRoute, RouterLink } from '@angular/router';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PublicWebsiteIndexComponent } from './index.component';
+import { QRCodeModule } from 'angularx-qrcode';
 
 describe('PublicWebsiteIndexComponent', () => {
   let component: PublicWebsiteIndexComponent;
   let fixture: ComponentFixture<PublicWebsiteIndexComponent>;
 
+  const fakeActivatedRoute = {
+    snapshot: { data: {} }
+  } as ActivatedRoute;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [PublicWebsiteIndexComponent]
+      declarations: [PublicWebsiteIndexComponent],
+      imports: [RouterLink, QRCodeModule],
+      providers: [
+        { provide: ActivatedRoute, useValue: fakeActivatedRoute }
+      ]
     });
     fixture = TestBed.createComponent(PublicWebsiteIndexComponent);
     component = fixture.componentInstance;
