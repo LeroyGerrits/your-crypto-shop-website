8f3aea56631e7caa70d6b8c5f5cb9cd292e7176f
Leroy Gerrits <leroygerrits@gmail.com>
2023-10-28 17:33:26 +0200

    Code coverage fixes

--- src/app/shared/dialogs/signup/dialog.signup.component.spec.ts
+++ src/app/shared/dialogs/signup/dialog.signup.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClient, HttpHandler } from '@angular/common/http';
 import { MAT_DIALOG_DATA, MatDialog, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { of, throwError } from 'rxjs';
 
 import { AccountComponent } from 'src/app/account/account.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -12,7 +13,6 @@ import { MerchantService } from '../../services/Merchant.service';
 import { MutationResult } from '../../models/MutationResult';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
-import { of } from 'rxjs';
 
 describe('DialogSignUpComponent', () => {
   let component: DialogSignUpComponent;
@@ -67,6 +67,65 @@ describe('DialogSignUpComponent', () => {
   });
 
   it('should create a new account when all the required fields are supplied', () => {
+    component.controlEmailAddress.setValue('merchant@dgbcommerce.com');
+    component.controlUsername.setValue('DGB Commerce');
+    component.controlLastName.setValue('Doe');
+    component.controlGender.setValue('0');
+    component.onSubmit();
+    expect(merchantServiceSpy.create).toHaveBeenCalled();
+  });
+
+  it('should show an error when handling submit result and an error code is applicable', () => {
+    const mutationResult = {
+      ErrorCode: 666,
+      Identity: '',
+      Message: 'Evil error'
+    };
+    component.handleOnSubmitResult(mutationResult);
+    expect(component.formError).toBe(mutationResult.Message);
+  });
+});
+
+describe('DialogSignUpComponentWithErrors', () => {
+  let component: DialogSignUpComponent;
+  let fixture: ComponentFixture<DialogSignUpComponent>;
+
+  let matDialogRefSpy: any;
+  let matDialogSpy: jasmine.SpyObj<MatDialog>
+  let merchantServiceSpy: jasmine.SpyObj<MerchantService>;
+  let mutationResult: MutationResult = { ErrorCode: 0, Identity: '', Message: '' };
+
+  beforeEach(() => {
+    matDialogRefSpy = jasmine.createSpyObj('MatDialogRef', ['close']);
+    matDialogRefSpy.componentInstance = { title: '', message: '' };
+    matDialogRefSpy.afterClosed = () => of(true);
+    matDialogRefSpy.close = () => of(true);
+
+    matDialogSpy = jasmine.createSpyObj('MatDialog', ['open']);
+    matDialogSpy.open.and.returnValue(matDialogRefSpy);
+
+    merchantServiceSpy = jasmine.createSpyObj('MerchantService', ['create']);
+    merchantServiceSpy.create.and.returnValue(throwError(() => new Error('ERROR')));
+
+    TestBed.configureTestingModule({
+      declarations: [DialogSignUpComponent],
+      imports: [BrowserAnimationsModule, MatDialogModule, MatFormFieldModule, MatInputModule, MatRadioModule, ReactiveFormsModule, RouterTestingModule.withRoutes(
+        [{ path: 'account', component: AccountComponent }]
+      )],
+      providers: [
+        { provide: MatDialogRef, useValue: matDialogRefSpy },
+        { provide: MAT_DIALOG_DATA, useValue: [] },
+        { provide: MerchantService, useValue: merchantServiceSpy },
+        HttpClient,
+        HttpHandler
+      ]
+    });
+    fixture = TestBed.createComponent(DialogSignUpComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should trigger error handling when sending a call to the merchant service and the request fails', () => {
     component.controlEmailAddress.setValue('merchant@dgbcommerce.com');
     component.controlUsername.setValue('DGB Commerce');
     component.controlLastName.setValue('Doe');
