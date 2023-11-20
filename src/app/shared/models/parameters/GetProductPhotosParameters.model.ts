3474e195ebd257ec7b5648ead7cc25d6426671af
Leroy Gerrits <leroygerrits@gmail.com>
2023-11-20 13:41:19 +0100

    ProductPhoto service getList

---
+++ src/app/shared/models/parameters/GetProductPhotosParameters.model.ts
@@ -0,0 +1,3 @@
+export class GetProductPhotosParameters {
+    ProductId?: string;
+}
--- src/app/shared/services/ProductPhoto.service.ts
+++ src/app/shared/services/ProductPhoto.service.ts
@@ -1,16 +1,28 @@
 import { HttpClient, HttpEvent, HttpParams } from '@angular/common/http';
 
 import { Environment } from 'src/app/shared/environments/Environment';
+import { GetProductPhotosParameters } from '../models/parameters/GetProductPhotosParameters.model';
 import { Injectable } from '@angular/core';
 import { MutateProductRequest } from '../models/request/MutateProductRequest.model';
 import { MutationResult } from 'src/app/shared/models/MutationResult';
 import { Observable } from 'rxjs';
+import { ProductPhoto } from '../models/ProductPhoto.model';
 
 @Injectable()
 export class ProductPhotoService {
     private apiUrl = Environment.API_URL + '/ProductPhoto';
 
     constructor(protected http: HttpClient) { }
+
+    getList(parameters?: GetProductPhotosParameters): Observable<ProductPhoto[]> {
+        let httpParams = new HttpParams();
+
+        if (parameters) {
+            if (parameters.ProductId) httpParams = httpParams.append('productId', parameters.ProductId);
+        }
+
+        return this.http.get<ProductPhoto[]>(this.apiUrl, { params: httpParams });
+    }
 
     upload(productId: string, formData: FormData): Observable<HttpEvent<MutationResult>> {
         return this.http.post<MutationResult>(`${this.apiUrl}?productId=${productId}`, formData, { reportProgress: true, observe: 'events' });
