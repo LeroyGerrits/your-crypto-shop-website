baa5968ea666d20e039952965473654480f91668
Leroy Gerrits <leroygerrits@gmail.com>
2024-01-08 23:02:08 +0100

    Unit test / code coverage

--- src/app/control-panel/configuration/pages/page.component.spec.ts
+++ src/app/control-panel/configuration/pages/page.component.spec.ts
@@ -1,29 +1,30 @@
-import { ActivatedRoute, Router, RouterLink, convertToParamMap } from '@angular/router';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute, Router, RouterLink, convertToParamMap } from '@angular/router';
 
-import { ControlPanelConfigurationPageComponent } from './page.component';
-import { TestDataPages } from 'src/assets/test-data/Pages';
-import { PageService } from 'src/app/shared/services/Page.service';
-import { ShopService } from 'src/app/shared/services/Shop.service';
-import { MatSnackBar } from '@angular/material/snack-bar';
-import { MutationResult } from 'src/app/shared/models/MutationResult';
-import { of, throwError } from 'rxjs';
-import { TestDataShops } from 'src/assets/test-data/Shops';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
-import { ReactiveFormsModule } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
+import { of, throwError } from 'rxjs';
+import { MutationResult } from 'src/app/shared/models/MutationResult';
+import { PageService } from 'src/app/shared/services/Page.service';
+import { ShopService } from 'src/app/shared/services/Shop.service';
+import { TestDataPages } from 'src/assets/test-data/Pages';
+import { TestDataShops } from 'src/assets/test-data/Shops';
+import { ControlPanelConfigurationPageComponent } from './page.component';
 
+import { MatCheckboxChange, MatCheckboxModule } from '@angular/material/checkbox';
 import { MatTabsModule } from '@angular/material/tabs';
-import { CategoryService } from 'src/app/shared/services/Category.service';
-import { TestDataCategories } from 'src/assets/test-data/Categories';
 import { MatTreeModule } from '@angular/material/tree';
 import { GetPageResponse } from 'src/app/shared/models/response/GetPageResponse.model';
-import { MatCheckboxChange, MatCheckboxModule } from '@angular/material/checkbox';
+import { PageCategoryService } from 'src/app/shared/services/PageCategory.service';
+import { TestDataCategories } from 'src/assets/test-data/Categories';
+import { TestDataPageCategories } from 'src/assets/test-data/PageCategories';
 import { ControlPanelConfigurationPageListComponent } from './page-list.component';
 
 describe('ControlPanelConfigurationPageComponent', () => {
@@ -32,14 +33,14 @@ describe('ControlPanelConfigurationPageComponent', () => {
 
   let matSnackBarSpy: jasmine.SpyObj<MatSnackBar>;
 
-  let categoryServiceSpy: jasmine.SpyObj<CategoryService>;
+  let pageCategoryServiceSpy: jasmine.SpyObj<PageCategoryService>;
   let pageServiceSpy: jasmine.SpyObj<PageService>;
   let shopServiceSpy: jasmine.SpyObj<ShopService>;
   let mutationResult: MutationResult = <MutationResult>{ ErrorCode: 0, Identity: '', Message: '' };
 
   beforeEach(() => {
-    categoryServiceSpy = jasmine.createSpyObj('CategoryService', ['getList']);
-    categoryServiceSpy.getList.and.returnValue(of(TestDataCategories));
+    pageCategoryServiceSpy = jasmine.createSpyObj('PageCategoryService', ['getList']);
+    pageCategoryServiceSpy.getList.and.returnValue(of(TestDataPageCategories));
 
     pageServiceSpy = jasmine.createSpyObj('PageService', ['getById', 'create', 'update']);
     pageServiceSpy.getById.and.returnValue(of(<GetPageResponse>{ Page: TestDataPages[0], CategoryIds: [TestDataCategories[0].Id] }));
@@ -54,13 +55,13 @@ describe('ControlPanelConfigurationPageComponent', () => {
     TestBed.configureTestingModule({
       declarations: [ControlPanelConfigurationPageComponent],
       imports: [BrowserAnimationsModule, MatCheckboxModule, MatDialogModule, MatDividerModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatTabsModule, MatTreeModule, ReactiveFormsModule, RouterLink, RouterTestingModule.withRoutes(
-        [{ path: 'control-panel/catalog/pages', component: ControlPanelConfigurationPageListComponent }]
+        [{ path: 'control-panel/configuration/pages', component: ControlPanelConfigurationPageListComponent }]
       )],
       providers: [
-        { provide: ActivatedRoute, useValue: { snapshot: { paramMap: convertToParamMap({ pageId: TestDataPages[0].Id, shopId: TestDataPages[0].ShopId }) } } },
-        { provide: CategoryService, useValue: categoryServiceSpy },
+        { provide: ActivatedRoute, useValue: { snapshot: { paramMap: convertToParamMap({ pageId: TestDataPages[0].Id, shopId: TestDataPages[0].Shop.Id }) } } },
         { provide: MatSnackBar, useValue: matSnackBarSpy },
         { provide: PageService, useValue: pageServiceSpy },
+        { provide: PageCategoryService, useValue: pageCategoryServiceSpy },
         { provide: ShopService, useValue: shopServiceSpy }
       ]
     });
@@ -70,7 +71,7 @@ describe('ControlPanelConfigurationPageComponent', () => {
   });
 
   it('should not proceed submitting when required form values are not filled in', () => {
-    component.controlName.setValue('');
+    component.controlTitle.setValue('');
     component.onSubmit();
     expect(component.formSubmitted).toBeTrue();
     expect(component.formLoading).toBeFalse();
@@ -78,22 +79,20 @@ describe('ControlPanelConfigurationPageComponent', () => {
 
   it('should send a call to the page service when creating a new page', () => {
     component.queryStringPageId = '';
-    component.queryStringShopId = TestDataPages[0].ShopId;
-    component.controlName.setValue(TestDataPages[0].Name);
-    component.controlShop.setValue(TestDataPages[0].ShopId);
-    component.controlPrice.setValue(TestDataPages[0].Price!.toString());
-    component.controlDescription.setValue(TestDataPages[0].Description!);
+    component.queryStringShopId = TestDataPages[0].Shop.Id;
+    component.controlTitle.setValue(TestDataPages[0].Title);
+    component.controlShop.setValue(TestDataPages[0].Shop.Id);
+    component.controlContent.setValue(TestDataPages[0].Content!);
     component.onSubmit();
     expect(component.formLoading).toBeFalse();
   });
 
   it('should send a call to the page service when updating an existing page', () => {
     component.queryStringPageId = TestDataPages[0].Id;
-    component.queryStringShopId = TestDataPages[0].ShopId;
-    component.controlName.setValue(TestDataPages[0].Name);
-    component.controlShop.setValue(TestDataPages[0].ShopId);
-    component.controlPrice.setValue(TestDataPages[0].Price!.toString());
-    component.controlDescription.setValue(TestDataPages[0].Description!);
+    component.queryStringShopId = TestDataPages[0].Shop.Id;
+    component.controlTitle.setValue(TestDataPages[0].Title);
+    component.controlShop.setValue(TestDataPages[0].Shop.Id);
+    component.controlContent.setValue(TestDataPages[0].Content!);
     component.onSubmit();
     expect(component.formLoading).toBeFalse();
   });
@@ -104,7 +103,7 @@ describe('ControlPanelConfigurationPageComponent', () => {
 
     const mutationResult = <MutationResult>{ Constraint: '', ErrorCode: 0, Identity: '', Message: '', Success: true };
     component.handleOnSubmitResult(mutationResult);
-    expect(routerstub.navigate).toHaveBeenCalledWith(['/control-panel/catalog/pages']);
+    expect(routerstub.navigate).toHaveBeenCalledWith(['/control-panel/configuration/pages']);
   });
 
   it('should show a message when an unhandled error occurs', () => {
@@ -137,14 +136,13 @@ describe('ControlPanelConfigurationPageComponentWithErrors', () => {
 
   let matSnackBarSpy: jasmine.SpyObj<MatSnackBar>;
 
-  let categoryServiceSpy: jasmine.SpyObj<CategoryService>;
+  let pageCategoryServiceSpy: jasmine.SpyObj<PageCategoryService>;
   let pageServiceSpy: jasmine.SpyObj<PageService>;
   let shopServiceSpy: jasmine.SpyObj<ShopService>;
-  let mutationResult: MutationResult = <MutationResult>{ ErrorCode: 0, Identity: '', Message: '' };
 
   beforeEach(() => {
-    categoryServiceSpy = jasmine.createSpyObj('CategoryService', ['getList']);
-    categoryServiceSpy.getList.and.returnValue(of(TestDataCategories));
+    pageCategoryServiceSpy = jasmine.createSpyObj('PageCategoryService', ['getList']);
+    pageCategoryServiceSpy.getList.and.returnValue(of(TestDataCategories));
 
     pageServiceSpy = jasmine.createSpyObj('PageService', ['getById', 'create', 'update']);
     pageServiceSpy.getById.and.returnValue(of(<GetPageResponse>{ Page: TestDataPages[0], CategoryIds: [''] }));
@@ -159,11 +157,11 @@ describe('ControlPanelConfigurationPageComponentWithErrors', () => {
     TestBed.configureTestingModule({
       declarations: [ControlPanelConfigurationPageComponent],
       imports: [BrowserAnimationsModule, MatCheckboxModule, MatDialogModule, MatDividerModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatTabsModule, MatTreeModule, ReactiveFormsModule, RouterLink, RouterTestingModule.withRoutes(
-        [{ path: 'control-panel/catalog/pages', component: ControlPanelConfigurationPageListComponent }]
+        [{ path: 'control-panel/configuration/pages', component: ControlPanelConfigurationPageListComponent }]
       )],
       providers: [
-        { provide: ActivatedRoute, useValue: { snapshot: { paramMap: convertToParamMap({ pageId: 'new', shopId: TestDataPages[0].ShopId }) } } },
-        { provide: CategoryService, useValue: categoryServiceSpy },
+        { provide: ActivatedRoute, useValue: { snapshot: { paramMap: convertToParamMap({ pageId: 'new', shopId: TestDataPages[0].Shop.Id }) } } },
+        { provide: PageCategoryService, useValue: pageCategoryServiceSpy },
         { provide: MatSnackBar, useValue: matSnackBarSpy },
         { provide: PageService, useValue: pageServiceSpy },
         { provide: ShopService, useValue: shopServiceSpy }
@@ -176,22 +174,20 @@ describe('ControlPanelConfigurationPageComponentWithErrors', () => {
 
   it('should trigger error handling when sending a call to the page service when creating a new page and the request fails', () => {
     component.queryStringPageId = '';
-    component.queryStringShopId = TestDataPages[0].ShopId;
-    component.controlName.setValue(TestDataPages[0].Name);
-    component.controlShop.setValue(TestDataPages[0].ShopId);
-    component.controlPrice.setValue(TestDataPages[0].Price!.toString());
-    component.controlDescription.setValue(TestDataPages[0].Description!);
+    component.queryStringShopId = TestDataPages[0].Shop.Id;
+    component.controlTitle.setValue(TestDataPages[0].Title);
+    component.controlShop.setValue(TestDataPages[0].Shop.Id);
+    component.controlContent.setValue(TestDataPages[0].Content!);
     component.onSubmit();
     expect(component.formLoading).toBeFalse();
   });
 
   it('should trigger error handling when sending a call to the page service when updating an page shop and the request fails', () => {
     component.queryStringPageId = TestDataPages[0].Id;
-    component.queryStringShopId = TestDataPages[0].ShopId;
-    component.controlName.setValue(TestDataPages[0].Name);
-    component.controlShop.setValue(TestDataPages[0].ShopId);
-    component.controlPrice.setValue(TestDataPages[0].Price!.toString());
-    component.controlDescription.setValue(TestDataPages[0].Description!);
+    component.queryStringShopId = TestDataPages[0].Shop.Id;
+    component.controlTitle.setValue(TestDataPages[0].Title);
+    component.controlShop.setValue(TestDataPages[0].Shop.Id);
+    component.controlContent.setValue(TestDataPages[0].Content!);
     component.onSubmit();
     expect(component.formLoading).toBeFalse();
   });
