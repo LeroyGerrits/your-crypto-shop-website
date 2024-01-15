cd9e788cc1159bd1cbf71c8e712d2c93028f4f5a
Leroy Gerrits <leroygerrits@gmail.com>
2024-01-15 19:48:54 +0100

    Localhost test URL change

--- src/app/shared/environments/Environment.ts
+++ src/app/shared/environments/Environment.ts
@@ -1,5 +1,5 @@
 export const Environment = {
-    API_URL: 'https://localhost:7170',
+    API_URL: 'https://localhost:44353',
     WEBSITE_DOMAIN: 'localhost',
     WEBSITE_URL: 'http://localhost:4200',
 }
